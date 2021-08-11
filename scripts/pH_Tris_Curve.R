d0b8176d9213c89075900fa66c4c116460bba565
Jill Ashey <jillashey@55.244.20.172.s.wireless.uri.edu>
2021-08-11 15:37:39 -0400

    updating daily meas

--- scripts/pH_Tris_Curve.R
+++ scripts/pH_Tris_Curve.R
@@ -6,12 +6,12 @@
 # modified for personal Lenovo PC at PT Whitney summer 2019; modified by SJG
 #setwd("~/Desktop/PutnamLab/Repositories/Astrangia_repo/Astrangia_repo/data/pH_Tris/") #set working directory
 
-Calib.Data <-read.table("data/pH_Tris/20210803.csv", header=TRUE, sep=",", na.string="NA", as.is=TRUE) #reads in the data files
+Calib.Data <-read.table("data/pH_Tris/20210810.csv", header=TRUE, sep=",", na.string="NA", as.is=TRUE) #reads in the data files
 model <-lm(mVTris ~ TTris, data=Calib.Data) #runs a linear regression of mV as a function of temperature
 coe <- coef(model) #extracts the coeffecients
 R2<-summary(model)$r.squared
 
-pdf("output/20210803.pdf") 
+pdf("output/pH_Tris/20210810.pdf") 
 plot(mVTris ~ TTris, data=Calib.Data)
 abline(lm(mVTris ~ TTris, data=Calib.Data))
 legend('topleft', legend = bquote(R^2 == .(format(R2, digits = 3))), bty='n')
