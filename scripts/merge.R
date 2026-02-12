99586a4cef3cfef364e7c0129bda54a3bf32bc96
Jill Ashey <jillashey@brb-03178.apn.wlan.private.upenn.edu>
2026-02-12 16:39:31 -0500

    network func enrichment, cleaning up repo

--- scripts/merge.R
+++
@@ -1,8 +0,0 @@
-color <- read.csv("../../../../ColorScore_Astrangia2021_Experimental.csv")
-head(color)
-
-
-
-color <- color %>% rename(colony_id = ID)
-test <- merge(color, pr.out, by = "colony_id")
-write.csv(test, "../../../../checks.csv")
